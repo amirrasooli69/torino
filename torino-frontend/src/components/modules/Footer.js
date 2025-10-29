bae1ad39b56cc401313e2c77e0c0ce1c7068a582
amirrasooli69 <amirrasooli69@gmail.com>
2025-10-29 13:17:30 +0330

    work on footer

--- torino-frontend/src/components/modules/Footer.js
+++ torino-frontend/src/components/modules/Footer.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+
+import darsad from "@/svg/darsad.svg";
+import support from "@/svg/support.svg";
+import heart from "@/svg/heart.svg";
 
 function Footer() {
   return (
     <div>
-        <hr className='text-gray-300 mt-8 mb-8'/>
-        <div></div>
-        <div></div>
-      
+      <hr className="text-gray-300 mt-8 mb-8 mx-10" />
+      <div className="flex flex-col">
+        <div className="flex flex-row-reverse gap-5">
+          <Image src={darsad} width={64} height={64} alt="darsad" />
+          <div>
+            <p className="text-[14px] font-medium text-right">بصرفه ترین قیمت</p>
+            <p>بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row-reverse gap-5">
+          <Image src={support} width={64} height={64} alt="darsad" />
+          <div>
+            <p className="text-[14px] font-medium text-right">پشتیبانی</p>
+            <p>پشتیبانی و همراهی 24 ساعته در تمامی مراحل سفر شما.</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row-reverse gap-5">
+          <Image src={heart} width={64} height={64} alt="darsad" />
+          <div>
+            <p className="text-[14px] font-medium text-right">رضایت کاربران</p>
+            <p>رضایت بیش از 10هزار کاربر از تور های ما. </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
